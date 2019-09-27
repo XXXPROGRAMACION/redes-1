78f113bb0d443605cd849425a68331366565d571
Victor Yrazusta Ibarra <victor.yrazusta@estudiante.uam.es>
2019-09-27 21:54:05 +0000

    Práctica 1 core dump

--- practica1.py
+++ practica1.py
@@ -1,11 +1,11 @@
-'''
+"""
     practica1.py
     Muestra el tiempo de llegada de los primeros 50 paquetes a la interfaz especificada
     como argumento y los vuelca a traza nueva con tiempo actual
 
     Autor: Javier Ramos <javier.ramos@uam.es>
     2019 EPS-UAM
-'''
+"""
 
 from rc1_pcap import *
 import sys
@@ -23,36 +23,36 @@ TO_MS = 10
 num_paquete = 0
 TIME_OFFSET = 30*60
 
-def signal_handler(nsignal,frame):
-	logging.info('Control C pulsado')
+def signal_handler(nsignal, frame):
+	logging.info("Control C pulsado")
 	if handle:
 		pcap_breakloop(handle)
 		
 
-def procesa_paquete(us,header,data):
+def procesa_paquete(us, header, data):
 	global num_paquete, pdumper
-	logging.info('Nuevo paquete de {} bytes capturado a las {}.{}'.format(header.len,header.ts.tv_sec,header.ts.tv_sec))
+	logging.info("Nuevo paquete de {} bytes capturado a las {}.{}".format(header.len, header.ts.tv_sec, header.ts.tv_sec))
 	num_paquete += 1
 	#TODO imprimir los N primeros bytes
 	#Escribir el tráfico al fichero de captura con el offset temporal
 	
 if __name__ == "__main__":
-	global pdumper,args,handle
-	parser = argparse.ArgumentParser(description='Captura tráfico de una interfaz ( o lee de fichero) y muestra la longitud y timestamp de los 50 primeros paquetes',
-	formatter_class=RawTextHelpFormatter)
-	parser.add_argument('--file', dest='tracefile', default=False,help='Fichero pcap a abrir')
-	parser.add_argument('--itf', dest='interface', default=False,help='Interfaz a abrir')
-	parser.add_argument('--nbytes', dest='nbytes', type=int, default=14,help='Número de bytes a mostrar por paquete')
-	parser.add_argument('--debug', dest='debug', default=False, action='store_true',help='Activar Debug messages')
+	global pdumper, args, handle
+	parser = argparse.ArgumentParser(description = "Captura tráfico de una interfaz (o lee de fichero) y muestra la longitud y timestamp de los 50 primeros paquetes",
+	formatter_class = RawTextHelpFormatter)
+	parser.add_argument("--file", dest = "tracefile", default = False, help = "Fichero pcap a abrir")
+	parser.add_argument("--itf", dest = "interface", default = False, help = "Interfaz a abrir")
+	parser.add_argument("--nbytes", dest = "nbytes", type = int, default = 14, help = "Número de bytes a mostrar por paquete")
+	parser.add_argument("--debug", dest = "debug", default = False, action = "store_true", help = "Activar Debug messages")
 	args = parser.parse_args()
 
 	if args.debug:
-		logging.basicConfig(level = logging.DEBUG, format = '[%(asctime)s %(levelname)s]\t%(message)s')
+		logging.basicConfig(level = logging.DEBUG, format = "[%(asctime)s %(levelname)s]\t%(message)s")
 	else:
-		logging.basicConfig(level = logging.INFO, format = '[%(asctime)s %(levelname)s]\t%(message)s')
+		logging.basicConfig(level = logging.INFO, format = "[%(asctime)s %(levelname)s]\t%(message)s")
 
 	if args.tracefile is False and args.interface is False:
-		logging.error('No se ha especificado interfaz ni fichero')
+		logging.error("No se ha especificado interfaz ni fichero")
 		parser.print_help()
 		sys.exit(-1)
 
@@ -63,18 +63,35 @@ if __name__ == "__main__":
 	pdumper = None
 	
 	#TODO abrir la interfaz especificada para captura o la traza
-	#TODO abrir un dumper para volcar el tráfico (si se ha especificado interfaz) 
+	#TODO abrir un dumper para volcar el tráfico (si se ha especificado interfaz)
 	
-	
-	
-	ret = pcap_loop(handle,50,procesa_paquete,None)
+	if args.interface is not False:
+		handle = pcap_open_live(args.interface, 1514, 1, 1, errbuf)
+		pdumper = pcap_dump_open(pcap_open_dead(DLT_EN10MB, 1514), args.tracefile)
+	else:
+		handle = pcap_open_offline(args.tracefile, errbuf)
+
+		if handle is None:
+			logging.error("Error al abrir el fichero " + str(args.tracefile))
+			parser.print_help()
+			sys.exit(-1)
+
+	logging.info("Patatillas 1")
+
+	ret = pcap_loop(handle, 50, procesa_paquete, None)
+
+	logging.info("Patatillas 2")
+
 	if ret == -1:
-		logging.error('Error al capturar un paquete')
+		logging.error("Error al capturar un paquete")
 	elif ret == -2:
-		logging.debug('pcap_breakloop() llamado')
+		logging.debug("pcap_breakloop() llamado")
 	elif ret == 0:
-		logging.debug('No mas paquetes o limite superado')
-	logging.info('{} paquetes procesados'.format(num_paquete))
+		logging.debug("No mas paquetes o limite superado")
+	logging.info("{} paquetes procesados".format(num_paquete))
 	#TODO si se ha creado un dumper cerrarlo
 	
+	pcap_close(handle)
+	if args.tracefile is False:
+		pcap_dump_close(pdumper)
 
